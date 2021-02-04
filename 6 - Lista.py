fcad5e437a7cfe9cd7b8fe89ea961a033d325cba
TheGabrielSN <63022715+TheGabrielSN@users.noreply.github.com>
2021-02-04 20:31:47 -0300

    Lista
    Manipulação da estrutura de dados lista

---
+++ 6 - Lista.py
@@ -0,0 +1,22 @@
+#
+"""
+Obs: Arquivo apenas informacional, não é recomendado a execução do mesmo, pois pode
+apresentar alguma mensagem de erro por conta da forma de apresentação
+"""
+#
+
+#########               Definição                ##############
+"""
+Listas em python é nada mais que uma estrutura de dado, onde pode se organizar qualquer tipo de dado
+em uma lista.
+
+A lista é definina com [], e seus dados podem ser de qualquer tipo, separados por virgula.
+Exemplo:
+    Lista = [] -> Lista vazia
+    numeros = [0,1,2,3,4,5,6,7,8,9]
+    vogais = ["a","e","i","o","u"]
+    lista = ["a", 0, 5.3, False]
+
+Seu tamanho é indefinido, limitando-se a memoria do dispositivo ao qual está sendo execultado o programa.
+"""
+
